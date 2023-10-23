13a35fa12e367f093e43fa70ca77f4ab7fa2a20d
Ueno Souza Silva <uenosouzasilva@gmail.com>
2023-10-23 20:56:51 -0300

    feat: adicionado a classe Conteudo

---
+++ desafios-java/abstracao-bootcamp/src/com/dio/desafio/domain/Conteudo.java
@@ -0,0 +1,30 @@
+package com.dio.desafio.domain;
+
+/**
+ *
+ * @author Ueno
+ */
+public abstract class Conteudo {
+
+    private String titulo;
+    private String descricao;
+
+    public abstract double calcularXp();
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
