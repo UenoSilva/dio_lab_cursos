129250627ecda1d1e47db12f70f72c028cda0bd6
Ueno Souza Silva <uenosouzasilva@gmail.com>
2023-11-11 12:26:44 -0300

    feat: adicionando o serviço MatriculaService

---
+++ desafios-java/academia-digital/src/main/java/com/dio/academia/service/MatriculaService.java
@@ -0,0 +1,20 @@
+package com.dio.academia.service;
+
+import java.util.List;
+
+import com.dio.academia.entity.Matricula;
+
+public interface MatriculaService {
+
+	// Criar uma nova matricula
+	Matricula create(MatriculaForm form);
+	
+	// retorna uma matricula com base no id passado
+	Matricula get(Long id);
+	
+	// retorna todas as matriculas
+	List<Matricula> getAll();
+	
+	// deleta uma matricula pelo id
+	void delete(Long id);
+}
