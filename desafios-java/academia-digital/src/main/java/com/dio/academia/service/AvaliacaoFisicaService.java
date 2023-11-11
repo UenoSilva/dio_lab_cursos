096f16f5ffacb14166f972caf8da57300dc31610
Ueno Souza Silva <uenosouzasilva@gmail.com>
2023-11-11 12:14:28 -0300

    feat: adicionando o serviço AvaliacaoFisicaService

---
+++ desafios-java/academia-digital/src/main/java/com/dio/academia/service/AvaliacaoFisicaService.java
@@ -0,0 +1,24 @@
+package com.dio.academia.service;
+
+import java.util.List;
+
+import com.dio.academia.entity.AvaliacaoFisica;
+
+public interface AvaliacaoFisicaService {
+
+	// criar uma avalição física e retorna a avalição física criada
+	AvaliacaoFisica create(AvaliacaoFisica avaliacaoFisica);
+	
+	// buscar uma avalição física pelo seu id
+	AvaliacaoFisica get(Long id);
+	
+	// retorna todas as avalições físicas
+	List<AvaliacaoFisica> getAll();
+	
+	// Alterar informações da avaliação fíca, utilizando o id para buscar a 
+	// avalição física e novas informações de avaliação físca
+	AvaliacaoFisica update(Long id, AvaliacaoFisicaUpdateForm formUpadate);
+	
+	// deleta uma avaliação físca pelo seu id
+	void delete(Long id);
+}
