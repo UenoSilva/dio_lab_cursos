bd340dbbe3bc09fa69fb8be88dc67272de6f3f38
Ueno Souza Silva <uenosouzasilva@gmail.com>
2023-11-12 17:13:20 -0300

    feat: adicionando anotations para as Entidades da pacote Entity

--- desafios-java/academia-digital/src/main/java/com/dio/academia/entity/Matricula.java
+++ desafios-java/academia-digital/src/main/java/com/dio/academia/entity/Matricula.java
@@ -2,10 +2,33 @@ package com.dio.academia.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_matricula")
 public class Matricula {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "aluno_id")
 	private Aluno aluno;
+
 	private LocalDateTime dataMatricula = LocalDateTime.now();
 
 }
