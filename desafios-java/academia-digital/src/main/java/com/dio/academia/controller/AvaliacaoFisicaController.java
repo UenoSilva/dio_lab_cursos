64540eec42e44c490e0e8738728cab6fdba96cf5
Ueno Souza Silva <uenosouzasilva@gmail.com>
2023-11-12 17:50:38 -0300

    feat: adicionando os controllers das entidades

---
+++ desafios-java/academia-digital/src/main/java/com/dio/academia/controller/AvaliacaoFisicaController.java
@@ -0,0 +1,10 @@
+package com.dio.academia.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/avaliacoes")
+public class AvaliacaoFisicaController {
+
+}
