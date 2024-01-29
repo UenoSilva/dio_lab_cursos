351ce187563d97f020a4eb988e7d5a1193b2fccf
UenoSilva <uenosouzasilva@gmil.com>
2024-01-29 14:18:00 -0300

    feat: adionando a função setupView

--- desafios-kotlin/portifolio-projetos/app/src/main/java/ueno/dio/githubsearch/ui/MainActivity.kt
+++ desafios-kotlin/portifolio-projetos/app/src/main/java/ueno/dio/githubsearch/ui/MainActivity.kt
@@ -2,11 +2,27 @@ package ueno.dio.githubsearch.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView
 import ueno.dio.githubsearch.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var userName: EditText
+    lateinit var btnComfirmar: Button
+    lateinit var repositories: RecyclerView
+    lateinit var githubApi: GitHubServices
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun setupView(){
+        userName = findViewById(R.id.et_name_user)
+        btnComfirmar = findViewById(R.id.bt_search)
+        repositories = findViewById(R.id.rv_repositories)
+    }
+
 }
