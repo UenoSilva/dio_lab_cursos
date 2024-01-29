878ac514d679ac987b226a3120d7b2d2e7f690cd
UenoSilva <uenosouzasilva@gmil.com>
2024-01-29 13:21:20 -0300

    feat: activity_main ajuste

--- desafios-kotlin/portifolio-projetos/app/src/main/java/ueno/dio/githubsearch/ui/MainActivity.kt
+++ desafios-kotlin/portifolio-projetos/app/src/main/java/ueno/dio/githubsearch/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package ueno.dio.githubsearch
+package ueno.dio.githubsearch.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ueno.dio.githubsearch.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
