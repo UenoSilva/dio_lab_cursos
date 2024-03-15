c4a9807f47eefdf1f9dba3b31fd203138d5e1e7d
UenoSilva <uenosouzasilva@gmil.com>
2024-03-15 16:32:03 -0300

    feat: criaando o projeto

---
+++ desafios-kotlin/to-do_list/app/src/main/java/com/ueno/todolistapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ueno.todolistapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ueno.todolistapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
