e5cebf220f4c695da76c409620e4c38dae98671f
UenoSilva <uenosouzasilva@gmil.com>
2024-06-05 12:19:15 -0300

    Work Manager para orquestrar as Notificações

--- desafios-kotlin/copa-2022-android-feature-base-project/app/src/main/kotlin/me/dio/copa/catar/MainActivity.kt
+++ desafios-kotlin/copa-2022-android-feature-base-project/app/src/main/kotlin/me/dio/copa/catar/MainActivity.kt
@@ -4,22 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import me.dio.copa.catar.extensions.observe
 import me.dio.copa.catar.features.MainScreen
 import me.dio.copa.catar.features.MainUiAction
 import me.dio.copa.catar.features.MainViewModel
+import me.dio.copa.catar.notification.scheduler.extensions.NotificationMatcherWorker
 import me.dio.copa.catar.notification.scheduler.extensions.showNotification
 import me.dio.copa.catar.ui.theme.Copa2022Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private val viewModel by viewModels<MainViewModel>()
@@ -27,7 +23,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
+        observeActions()
 
         setContent {
             Copa2022Theme {
